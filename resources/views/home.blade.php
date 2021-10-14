fbe45563f69af5bf917e8dc3e46d7a4f219e23e0
isisfa <isis.falves@gmail.com>
2021-10-14 20:11:50 +0100

    items on cards, add images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,59 @@
         <a href="#" class="btn btn-sm"><i class="fa fa-shopping-cart"></i></a>
     </nav>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/product1.png') }}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 1</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/product2.png') }}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 2</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/product3.png') }}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 3</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/product4.png') }}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h6 class="card-title">Produto 4</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/product5.png') }}" class="card-img-top" alt="">                    <div class="card-body">
+                        <h6 class="card-title">Produto 5</h6>
+                        <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
