1f72ddd43f98eae9319cd3693f7f0960bc0557a3
isisfa <isis.falves@gmail.com>
2021-10-15 14:32:58 +0100

    add Rmodel to models

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-class Pedido extends Model
+class Pedido extends RModel
 {
 
 }
--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-class Produto extends Model
+class Produto extends RModel
 {
 
 }
