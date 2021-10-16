36d6f74429d5b2740a659badeb3728cfd5ab804a
isisfa <isis.falves@gmail.com>
2021-10-16 21:16:34 +0100

    password protection

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -20,15 +20,21 @@ class ClienteController extends Controller
         $usuario = new Usuario($values);
         //ISOLADAMENTE: $usuario->cpf = $request->input("cpf", "");
         $usuario->login = $request->input("cpf", "");
+        //Proteção senha Hash para criptografias
+        $senha = $request->input("password", "");
+        $usuario->password = \Hash::make($senha);
+
         $endereco = new Endereco($values);
         $endereco->logradouro = $request->input("endereco", "");
 
         try{
+            \DB::beginTransaction();
             $usuario->save();
             $endereco->usuario_id = $usuario->id; //relaciona as tabelas
             $endereco->save();
+            \DB::commit();
         }catch(\Exception $e){
-
+            \DB::rollback();
         }
 
         return redirect()->route('cadastrar');
